950b15f1efc981eb7f8dcc1df16b9b24fda70931
diegolr-avanade <diego.lopez.reduello@avanade.com>
2024-04-03 10:24:49 +0200

    ✨Detectar números

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -10,6 +10,25 @@ namespace TestProject
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("<---Calculadora--->");
+
+            double a, b;
+            char op;
+
+            Console.WriteLine("Inserte el primer dígito:");
+            a = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Inserte el segundo dígito:");
+            b = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Tus numeros son: {a} y {b}");
+            WaitForInput();
+        }
+
+        static void WaitForInput()
+        {
+            Console.WriteLine("\nPrograma terminado,\npulse una tecla para cerrar...");
+            Console.ReadLine();
         }
     }
 }
